74bb4d8247a005063a62c37dbc600410d13f8074
BeaverlyGithub <beaverlyapp@gmail.com>
2025-07-17 01:53:55 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -254,7 +254,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 verificationText.textContent = 'Email verified';
             }
         }
-    }
+    
 
 
     function initializeModals() {
@@ -324,6 +324,7 @@ verifyEmailBtn?.addEventListener('click', async () => {
     }
 });
 
+    }
     }
 
    async function handleapiConnection(e) {
